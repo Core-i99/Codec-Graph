eedd9c323dc92cda7152dda1c14d635562f161fa
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2023-03-15 19:42:39 +0100

    Put legend into label (top-left)

--- Codec-Graph.py
+++ Codec-Graph.py
@@ -39,7 +39,7 @@ from tkinter import filedialog
 Version = "V1.4"
 ALL_NODES = False
 outputname = "codecdump"
-outputfilename = outputname + ".svg"
+outputfilename = outputname + ".png"
 documents_dir = os.path.join(os.path.expanduser("~"), "Documents")
 tmp_dir = tempfile.mkdtemp()
 
@@ -146,13 +146,13 @@ def openFileClicked():
             ci.dump_graph()
 
         # open file dialog
-        f = filedialog.asksaveasfile(initialfile='Codec-Dump.svg', initialdir=documents_dir,
-                                     defaultextension=".svg", filetypes=[("SVG files", "*.svg")])
+        f = filedialog.asksaveasfile(initialfile='Codec-Dump.png', initialdir=documents_dir,
+                                     defaultextension=".png", filetypes=[("png files", "*.png")])
         if f != None:
             # running graphviz
             # usage of graphviz (dot): dot -T$extention -o$outfile.$extention $inputfile
             rungraphviz = os.system(
-                f'dot -Tsvg -o"{f.name}" {dotfile}')
+                f'dot -Tpng -o"{f.name}" {dotfile}')
 
             if rungraphviz == 0:
                 logging.info("Running Graphviz succeed")
@@ -186,6 +186,7 @@ fm3.pack(pady=10)
 fm4.pack(pady=10)
 
 dotfile = os.path.join(tmp_dir, "dotfile.txt")
+print(dotfile)
 logging.info("Dotfile path %s", dotfile)
 
 
@@ -686,21 +687,46 @@ class CodecInfo:
     def dump_graph(self):
         with open(dotfile, "w", encoding='utf-8') as file:
             file.write('digraph {\n')
-            file.write("""
+            file.write(f"""
             rankdir=RL
             ranksep=3.0
             labelloc="t";
-            label=""" + f'"Codec: {self.fields["Codec"]}\nVendor Id: {self.fields["Vendor Id"]}\nSubsystem Id: {self.fields["Subsystem Id"]}\nRevision Id: {self.fields["Revision Id"]}"' + """;
-            node [shape=plaintext]
-            subgraph cluster_legend { 
-                label = "Node shape and colors";
-                n1 [label=AMP shape=triangle orientation=90 color="#0000ff"]
-                n2 [label="Pin Complex" shape=box  color=green]
-                n3 [label="Amp In" color=red shape=ellipse]
-                n4 [label="Amp Out" color=blue shape=ellipse]
-                n5 [label="Audio Selector" shape=parallelogram]
-                n6 [label="Audio Mixer" shape=hexagon]
-            }
+            labeljust=l;
+            label=<<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0" CELLPADDING="4">
+                <TR>
+                    <TD>
+                        <FONT POINT-SIZE="12"><B>Codec: </B>{self.fields["Codec"]}</FONT>
+                    </TD>
+                </TR>
+                <TR>
+                    <TD>
+                        <FONT POINT-SIZE="12"><B>Codec Id: </B>{int(self.fields["Vendor Id"], 16)} ({self.fields["Vendor Id"]})</FONT>
+                    </TD>
+                </TR>
+                <TR>
+                    <TD>
+                        <FONT POINT-SIZE="12"><B>Address: </B>{self.fields["Address"]}</FONT>
+                    </TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/Amp.png" /></TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/PinComplex.png" /></TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/AmpIn.png" /></TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/AmpOut.png" /></TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/AudioSelector.png" /></TD>
+                </TR>
+                <TR>
+                    <TD><IMG SRC="images/AudioMixer.png" /></TD>
+                </TR>
+            </TABLE>>;
             """)
             for n in list(self.nodes.values()):
                 n.dump_graph(file)
